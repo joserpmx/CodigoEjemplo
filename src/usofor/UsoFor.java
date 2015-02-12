a5bc8db0559c9204938dd64d2e5f1acbff5f8840
jcdurazo <jc_durazo@hotmail.com>
2015-02-12 11:47:01 -0700

    Update UsoFor.java

--- src/usofor/UsoFor.java
+++ src/usofor/UsoFor.java
@@ -15,6 +15,7 @@ public class UsoFor {
      */
      //agrego comentario
     //Agrego otro Comentario
+    //Mi comentario JCDM
     public static void main(String[] args) {
         // TODO code application logic here
         int acumulador=0;
